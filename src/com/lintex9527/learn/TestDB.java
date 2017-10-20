2140b5b748061243801074e463ec4c4c07471749
lintex9527 <lintex9527@yeah.net>
2017-10-20 12:01:26 +0800

    JDBC 实例更新操作完成。

--- src/com/lintex9527/learn/TestDB.java
+++ src/com/lintex9527/learn/TestDB.java
@@ -1,15 +1,10 @@
 package com.lintex9527.learn;
 
-import com.mysql.jdbc.PreparedStatement;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -40,9 +35,9 @@ public class TestDB {
     /**
      * 一个查询操作的例子
      */
-    public static void test_read(){
+    public static void test_sql(){
         // 声明 Connection 对象
-        Connection con;
+        Connection connection;
 
         // TODO: 用 JSON 格式文件保存用户名和密码
         // MySQL 配置时的用户名和密码
@@ -54,95 +49,137 @@ public class TestDB {
             // 加载驱动程序
             Class.forName(DRIVER);
             // 1. getConnection() 方法，连接到数据库
-            con = DriverManager.getConnection(URL, user, password);
-            if (!con.isClosed()){
-                System.out.println("Succeeded to connect to the database.");
+            connection = DriverManager.getConnection(URL, user, password);
+            if (!connection.isClosed()){
+                System.out.println("成功连接至数据库。");
             }
 
             // 2. 创建 Statement 类对象，用来执行 SQL 语句
-            Statement statement = con.createStatement();
-            // 要执行的SQL语句
-            String sql = "SELECT * FROM emp";
+            Statement statement = connection.createStatement();
+            // SQL 中的 UPDATE, DELETE 等语句使用这个
+            PreparedStatement psql;
 
-            // 3. ResultSet 类，用来存放获取的结果集
-            ResultSet rs = statement.executeQuery(sql);
-            System.out.println("-------------------");
-            System.out.println("执行结果如下：");
-            System.out.println("姓名\t职称");
-            System.out.println("-------------------");
-
+            int empno = 0;
+            String ename = null;
+            float sal = (float)0.0;
             String job = null;
-            String id = null;
-            while (rs.next()){
-                job = rs.getString("job");
-                id = rs.getString("ename");
 
-                System.out.println(id + "\t" + job);
+            // 跟来产生随机数，用做 empno，或者 sal 增加的值
+            Random random = new Random();
+
+            boolean flag = true;
+            while (flag){
+                // 用按键选择对应的操作
+                BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("--------------------------------------------------");
+                String prompt = "1、查询\n2、插入\n3、更新\n4、删除\n5、退出";
+                System.out.println(prompt);
+                System.out.print("选择对应的操作：");
+                String ans = bufReader.readLine();
+                System.out.println("选择的是： " + ans);
+                System.out.println("--------------------------------------------------");
+                int num = Integer.parseInt(ans.trim(), 10);
+
+                switch (num){
+                    case 1:
+                        // 查询
+
+                        // 要执行的SQL语句
+                        String sql = "SELECT * FROM emp ORDER BY sal";
+                        // 3. ResultSet 类，用来存放获取的结果集
+                        ResultSet rs = statement.executeQuery(sql);
+                        System.out.println("查询结果如下：");
+                        System.out.println("工号\t姓名\t职称\t工资");
+                        System.out.println("----------------------------------------");
+
+                        while (rs.next()){
+                            empno = rs.getInt("empno");
+                            ename = rs.getString("ename");
+                            job = rs.getString("job");
+                            sal = rs.getFloat("sal");
+                            System.out.println(empno + "\t" + ename + "\t" + job + "\t" + sal);
+                        }
+                        // 最后必须关闭连接
+                        rs.close();
+
+                        break;
+                    case 2:
+                        // 插入
+                        // 9935 李晓峰 人事  2015-03-01  3520.00
+
+                        // 预处理添加数据
+                        psql = (PreparedStatement) connection.prepareStatement("INSERT INTO emp (empno, ename, job, hiredate, sal)"
+                                + "values(?, ?, ?, ?, ?)");
+
+                        empno = random.nextInt(10000);
+                        psql.setInt(1, empno);
+
+                        psql.setString(2, "李晓峰");
+                        psql.setString(3, "人事");
+
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        Date mydate = dateFormat.parse("2015-03-1");
+                        psql.setDate(4, new java.sql.Date(mydate.getTime()));
+                        psql.setFloat(5, (float) 3520.00);
+                        psql.executeUpdate();
+
+                        break;
+                    case 3:
+                        // 更新
+                        // 给 sal 最低的雇员 sal += 666.00
+                        String sql_lowest_sal = "SELECT * FROM emp ORDER BY sal LIMIT 1";
+                        ResultSet resultSet = statement.executeQuery(sql_lowest_sal);
+                        // 记住，只要是 ResultSet 就必须使用固定的如下的格式访问其中的每一个记录，哪怕只有一个记录
+                        // while(resultSet.next()){}
+                        while(resultSet.next()){
+                            empno = resultSet.getInt("empno");
+                            sal = resultSet.getFloat("sal");
+                            int add = random.nextInt(1000);
+                            System.out.println("随机增加了： " + add);
+                            sal += add;
+
+                            psql = connection.prepareStatement("UPDATE emp SET sal = ? WHERE empno = ?");
+                            psql.setFloat(1, sal);
+                            psql.setInt(2, empno);
+                            psql.executeUpdate();
+                        }
+                        
+                        break;
+                    case 4:
+                        // 删除
+
+                        break;
+                    case 5:
+                        // 退出
+                    default:
+                        // 默认也是退出
+                        flag = false;
+                        break;
+                }
             }
+
+
+
+
+
 
             // 最后必须关闭连接
-            rs.close();
-            con.close();
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("查询操作结束。");
-        }
-
-    }
-
-    /**
-     * 数据库操作--更新
-     * 插入一个记录
-     * 9935 李晓峰 人事  2015-03-01  3520.00
-     */
-    public static void test_update(){
-        // 声明 Connection 对象
-        Connection connection;
-
-        // MySQL 用户名和口令
-        String username = getString("输入MySQL 用户名：");
-        String password = getString("输入其口令：");
-
-        try {
-            Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL, username, password);
-            if (! connection.isClosed()){
-                System.out.println("成功连接至数据库");
-            }
-            PreparedStatement psql;
-            // 预处理添加数据
-            psql = (PreparedStatement) connection.prepareStatement("INSERT INTO emp (empno, ename, job, hiredate, sal)"
-                                            + "values(?, ?, ?, ?, ?)");
-            Random random = new Random();
-            int empno = random.nextInt(10000);
-            psql.setInt(1, empno);
-
-            psql.setString(2, "李晓峰");
-            psql.setString(3, "人事");
-
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            Date mydate = dateFormat.parse("2015-03-1");
-            psql.setDate(4, new java.sql.Date(mydate.getTime()));
-            psql.setFloat(5, (float) 3520.00);
-            psql.executeUpdate();
-
-            // Connection 最后必须关闭
             connection.close();
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
         } finally {
-            System.out.println("插入操作结束。");
+            System.out.println("操作结束。");
         }
+
     }
+
 
     /**
      * 提示用户输入一行字符
@@ -167,9 +204,7 @@ public class TestDB {
     }
 
     public static void main(String[] args) {
-//        test_read();
-        test_update();
-//        test_read();
+        test_sql();
     }
 
 }
